edd9edb69660f17f5b1d555823170689e4e66347
Naveen Chinthapally <naveen.chinthapally@snowflake.com>
2023-02-08 22:12:01 +0530

    Commit test.sql

---
+++ Sample_DDL/test.sql
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2708
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 create or replace table employees ("firstname" string, "LASTNAME" string, phone string, location string, zip number, active boolean);\
+insert into employees\
+  values\
+  ('Lysandra','Reeves','1-212-759-3751','New York',10018, 'false'),\
+  ('Michael','Arnett','1-650-230-8467','San Francisco',94116, 'true'),\
+  ('Jon','Snow','','New York',10018, 'false'),\
+  ('Xin','Huang','911','Bellevue',98006, 'true');}
