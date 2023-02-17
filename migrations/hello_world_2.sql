ac45ce2eb8989c432966ca3d92016708c0cb3314
Naveen Chinthapally <naveen.chinthapally@snowflake.com>
2023-02-17 08:43:39 +0530

    Create hello_world_2.sql

---
+++ migrations/hello_world_2.sql
@@ -0,0 +1,11 @@
+use database NCHIN_DB;
+use schema TEST_DEVOPS;
+use warehouse NCHIN_M_WH;
+use role sysadmin;
+CREATE SCHEMA if not exists TEST_DEVOPS;
+CREATE TABLE if not exists HELLO_WORLD_2
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+  ,MIDDLE_NAME VARCHAR
+);
