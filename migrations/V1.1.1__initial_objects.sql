7c252e072720051a4ee883d686c5db6ff4c8c251
Naveen Chinthapally <naveen.chinthapally@snowflake.com>
2023-02-17 08:33:37 +0530

    Update V1.1.1__initial_objects.sql

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,7 +1,10 @@
+use database NCHIN_DB;
+use schema TEST_DEVOPS;
+use warehouse NCHIN_M_WH;
+use role sysadmin;
 CREATE SCHEMA if not exists TEST_DEVOPS;
 CREATE TABLE if not exists HELLO_WORLD
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
 );
- 
